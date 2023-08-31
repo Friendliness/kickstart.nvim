8ba1fbd2a1819c56618a6d04fa9a4085628d07b1
Friendliness <friendliness@friendliness.me>
2023-08-31 09:19:13 +0200

    Most modified keymaps have been moved to a separate file

---
+++ lua/custom/keymaps.lua
@@ -0,0 +1,18 @@
+local keymap = vim.keymap.set
+-- Keymap for toggling the tree - Neo-Tree
+keymap('n', '<C-n>', ':Neotree <CR>', { desc = 'Toggle neotree' })
+
+-- Switch between windows
+
+keymap('n', '<C-h>', '<C-w>h', { desc = 'Window left' })
+keymap('n', '<C-l>', '<C-w>l', { desc = 'Window right' })
+keymap('n', '<C-j>', '<C-w>j', { desc = 'Window down' })
+keymap('n', '<C-k>', '<C-w>k', { desc = 'Window up' })
+
+-- Buffers Tab
+keymap('n', '<Tab>', ':BufferNext<CR>', { desc = 'Next buffer' })
+keymap('n', '<S-Tab>', ':BufferPrevious<CR>', { desc = 'Previous buffer' })
+keymap('n', '<leader>x', ':BufferClose<CR>', { desc = 'Close buffer' })
+
+-- Lazygit
+keymap('n', '<leader>gs', ':LazyGit<CR>', { desc = 'LazyGit' })
