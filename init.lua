77d6e6ea0a52fe48403131fbdb34abaf9c8da2a8
Friendliness <friendliness@friendliness.me>
2023-12-14 15:16:05 +0100

    Removed some shenanigans

--- init.lua
+++ init.lua
@@ -127,12 +127,6 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
-        -- <<<<<<< HEAD
-        --         vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
-        --           { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-        --         vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-        --         vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
-        -- =======
         local gs = package.loaded.gitsigns
 
         local function map(mode, l, r, opts)
