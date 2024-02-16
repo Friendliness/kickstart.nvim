cc0b0dba946aacf77b4d1e2cbbf2ee0fc64ae859
Friendliness <boogwim@gmail.com>
2024-02-16 18:27:23 +0100

    snippet engine

--- lua/custom/plugins/neogen.lua
+++ lua/custom/plugins/neogen.lua
@@ -2,8 +2,9 @@ return {
   "danymat/neogen",
   dependencies = "nvim-treesitter/nvim-treesitter",
   config = true,
+  snippet_engine = "luasnip"
   -- Uncomment next line if you want to follow only stable versions
   -- version = "*"
 }
 
-require('neogen').setup({ snippet_engine = "luasnip" })
+-- require 'neogen'.setup({ snippet_engine = "luasnip" })
