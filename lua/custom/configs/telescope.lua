761d75296649a3e6938f57238f570a3207b3b04a
Friendliness <boogwim@gmail.com>
2024-02-22 19:44:54 +0100

    Testing for now with find files to show hidden

--- lua/custom/configs/telescope.lua
+++ lua/custom/configs/telescope.lua
@@ -1,9 +1,10 @@
--- An idea to modify the find files default to show hidden files
+-- An idea to modify the find_files default to show hidden files and files ignored by .gitignore
 local telescope = require('telescope')
 telescope.setup({
 	pickers = {
 		find_files = {
-			hidden = true
+			hidden = true,
+			no_ignore = true,
 		}
 	}
 })
