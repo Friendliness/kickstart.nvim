20846cd5647aac918c08c065f31c64f42ce5f40b
Friendliness <friendliness@friendliness.me>
2024-05-22 16:40:13 +0200

    Added silicon and keymaps for it with a window title

--- lua/custom/plugins/silicon.lua
+++ lua/custom/plugins/silicon.lua
@@ -6,7 +6,12 @@ return {
     require('silicon').setup {
       -- Configuration here, or leave empty to use defaults
       font = 'Hack=34',
+      theme = 'Dracula',
+      no_window_controls = true,
       to_clipboard = true,
+      window_title = function()
+        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ':t')
+      end,
       output = function()
         return '~/Pictures/code-screenshots/' .. os.date '!%Y-%m-%dT%H-%M-%S' .. '_code.png'
       end,
