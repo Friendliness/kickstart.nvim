b5365a8c7490db100cfebb3be8b48b00e2221df7
Friendliness <friendliness@friendliness.me>
2024-11-19 08:41:32 +0100

    Added session plugin

--- lua/custom/plugins/persistence.lua
+++ lua/custom/plugins/persistence.lua
@@ -1,7 +1,5 @@
 return {
-  'folke/persistence.nvim.git',
-  event = 'BufReadPre', -- this will only start session saving when an actual file was opened
-  opts = {
-    -- add any custom options here
-  },
+  'folke/persistence.nvim',
+  event = 'BufReadPre',
+  opts = {},
 }
