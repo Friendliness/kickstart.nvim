bd01c93cceb6025f1c4206b0ece285efc50f94bc
Friendliness <friendliness@friendliness.me>
2025-02-18 12:15:59 +0100

    Fixed blink keymappig

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -19,9 +19,8 @@ return {
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- see the "default configuration" section below for full documentation on how to define
       -- your own keymap.
-      keymap = {
-        preset = 'default',
-        cmdline = {
+      cmdline = {
+        keymap = {
           preset = 'enter',
           ['<C-y>'] = { 'accept', 'fallback' },
           ['<CR>'] = { 'accept', 'fallback' },
