6663a83704d3a296772a2513210d69fd0b92abac
Friendliness <friendliness@friendliness.me>
2025-02-25 12:28:44 +0100

    Added more snacks plugins

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -10,6 +10,13 @@ return {
     bigfile = { enabled = true },
     dashboard = {
       enabled = true,
+      sections = {
+        { section = 'header' },
+        { section = 'keys', gap = 1 },
+        { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = { 2, 2 } },
+        { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 2 },
+        { section = 'startup' },
+      },
     },
     debug = { enabled = true },
     indent = { enabled = true },
