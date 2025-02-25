eed34cf841dd64a3205ca9a7fd57659bb2973000
Friendliness <friendliness@friendliness.me>
2025-02-25 12:41:36 +0100

    Added configuration for snacks dashboard

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -13,8 +13,22 @@ return {
       sections = {
         { section = 'header' },
         { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1, { action = '<leader>sf', key = 'f' } },
-        { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
-        { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+        { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1, pane = 2 },
+        { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1, pane = 2 },
+        {
+          pane = 2,
+          icon = ' ',
+          title = 'Git Status',
+          section = 'terminal',
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = 'git status --short --branch --renames',
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
         { section = 'startup' },
       },
     },
