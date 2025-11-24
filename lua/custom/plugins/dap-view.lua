f4a2528bbb9fa38b6f7d43ed88dfa69ce8e62488
Friendliness <friendliness@friendliness.me>
2025-11-24 11:08:34 +0100

    Removed legacy dap view action

--- lua/custom/plugins/dap-view.lua
+++ lua/custom/plugins/dap-view.lua
@@ -12,65 +12,41 @@ return {
             keymap = '<M-b>',
             label = 'Breakpoints [M-b]',
             short_label = ' [M-b]',
-            action = function()
-              require('dap-view.views').switch_to_view 'breakpoints'
-            end,
           },
           scopes = {
             keymap = '<M-s>',
             label = 'Scopes [M-s]',
             short_label = '󰂥 [M-s]',
-            action = function()
-              require('dap-view.views').switch_to_view 'scopes'
-            end,
           },
           exceptions = {
             keymap = '<M-e>',
             label = 'Exceptions [M-e]',
             short_label = '󰢃 [M-e]',
-            action = function()
-              require('dap-view.views').switch_to_view 'exceptions'
-            end,
           },
           watches = {
             keymap = '<M-w>',
             label = 'Watches [M-w]',
             short_label = '󰛐 [M-w]',
-            action = function()
-              require('dap-view.views').switch_to_view 'watches'
-            end,
           },
           threads = {
             keymap = '<M-t>',
             label = 'Threads [M-t]',
             short_label = '󱉯 [M-t]',
-            action = function()
-              require('dap-view.views').switch_to_view 'threads'
-            end,
           },
           repl = {
             keymap = '<M-r>',
             label = 'REPL [M-r]',
             short_label = '󰯃 [M-r]',
-            action = function()
-              require('dap-view.repl').show()
-            end,
           },
           sessions = {
             keymap = '<M-k>', -- I ran out of mnemonics
             label = 'Sessions [M-k]',
             short_label = ' [M-k]',
-            action = function()
-              require('dap-view.views').switch_to_view 'sessions'
-            end,
           },
           console = {
             keymap = '<M-c>',
             label = 'Console [M-c]',
             short_label = '󰆍 [M-c]',
-            action = function()
-              require('dap-view.views').switch_to_view 'console'
-            end,
           },
         },
       },
