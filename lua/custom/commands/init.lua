153aab606e70bc6ad57f8dbdc2f8c25668379adb
Friendliness <friendliness@friendliness.me>
2026-01-24 19:22:30 +0100

    Corrected dap-view icons and added autocommand for relative number etc

--- lua/custom/commands/init.lua
+++ lua/custom/commands/init.lua
@@ -4,3 +4,15 @@ end, {
   nargs = '+',
   desc = 'Dump the output of a command at the cursor position',
 })
+
+-- Automatically toggle relative line numbers based on mode
+vim.api.nvim_create_autocmd('ModeChanged', {
+  pattern = '*',
+  callback = function(ev)
+    if string.match(ev.match, '.*:i') then
+      vim.opt.relativenumber = false
+    elseif string.match(ev.match, '.*:n') then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
