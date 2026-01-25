0fc7aea4bb50a6aefe0ebb46f9ee4ea09bdb5949
Friendliness <friendliness@friendliness.me>
2026-01-25 19:43:49 +0100

    Removed a bunch of plguins

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_set_keymap('i', '<M-l>', 'copilot#Accept("<CR>")', { expr = true, noremap = true, silent = true })
 vim.g.copilot_no_tab_map = true
-vim.cmd [[autocmd! BufRead,BufNewFile *.env,*.env.*,*.env-* set filetype=dotenv]]
+vim.cmd [[autocmd! BufRead,BufNewFile *.env,*.env.*,*.env-*,.env* set filetype=dotenv]]
 return {
   'github/copilot.vim',
 }
