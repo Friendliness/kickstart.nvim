21e22be0e47814e76028ad055a09ea8045fd3bf7
Friendliness <friendliness@friendliness.me>
2026-01-30 11:02:20 +0100

    Added new command and restricted modechanged to normal buffers

--- lua/custom/commands/init.lua
+++ lua/custom/commands/init.lua
@@ -10,6 +10,9 @@ end, {
 vim.api.nvim_create_autocmd('ModeChanged', {
   pattern = '*',
   callback = function(ev)
+    if vim.bo.buftype ~= '' then
+      return
+    end
     if string.match(ev.match, '.*:i') then
       vim.opt.relativenumber = false
     elseif string.match(ev.match, '.*:n') then
@@ -27,3 +30,23 @@ vim.api.nvim_create_user_command('QfFilter', function(opts)
   end, qf)
   vim.fn.setqflist(filtered, 'r')
 end, { nargs = 1, desc = 'Filter quickfix list by pattern' })
+
+-- Filter the location list based on a pattern, for example when using LSP diagnostics
+vim.api.nvim_create_user_command('LlFilter', function(opts)
+  local pattern = opts.args
+  local ll = vim.fn.getloclist(0)
+  local filtered = vim.tbl_filter(function(item)
+    return string.match(item.text, pattern) ~= nil
+  end, ll)
+  vim.fn.setloclist(0, filtered, 'r')
+end, { nargs = 1, desc = 'Filter location list by pattern' })
+
+-- Command to print buffer information
+vim.api.nvim_create_user_command('BufInfo', function()
+  local info = {
+    buftype = vim.bo.buftype,
+    filetype = vim.bo.filetype,
+    bufname = vim.fn.bufname(),
+  }
+  print(vim.inspect(info))
+end, {})
