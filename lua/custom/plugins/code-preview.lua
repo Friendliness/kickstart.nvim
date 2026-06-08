fbb8d1bc7ca09644cb05c510918f46029d5878f7
Friendliness <friendliness@friendliness.me>
2026-06-08 20:46:56 +0200

    Added code preview

---
+++ lua/custom/plugins/code-preview.lua
@@ -0,0 +1,12 @@
+return {
+  'Cannon07/code-preview.nvim',
+
+
+  config = function()
+    require('code-preview').setup {
+      diff = {
+        layout = 'tab', -- or "vsplit" / "inline"
+      },
+    }
+  end,
+}
