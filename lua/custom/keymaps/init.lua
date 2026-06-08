c2916a42c58dabd5108ef634a4352c98d8de7a39
Friendliness <friendliness@friendliness.me>
2026-06-08 19:23:58 +0200

    Revert codecompanion removal

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -182,9 +182,9 @@ keymap('n', '<leader>mt', '<Cmd>Markview toggle<CR>', { desc = 'Markview Toggle'
 keymap('x', '<M-/>', '<Esc>/\\%V', { desc = 'Search inside of selection' })
 
 -- CodeCompanion
--- keymap('n', '<leader>cci', '<Cmd>CodeCompanion<CR>', { desc = '[C]ode[C]ompanion [I]nline' })
--- keymap('n', '<leader>cca', '<Cmd>CodeCompanionAction<CR>', { desc = '[C]ode[C]ompanion [A]ction' })
--- keymap('n', '<leader>ccc', '<Cmd>CodeCompanionChat<CR>', { desc = '[C]ode[C]ompanion [C]hat' })
+keymap('n', '<leader>cci', '<Cmd>CodeCompanion<CR>', { desc = '[C]ode[C]ompanion [I]nline' })
+keymap('n', '<leader>cca', '<Cmd>CodeCompanionAction<CR>', { desc = '[C]ode[C]ompanion [A]ction' })
+keymap('n', '<leader>ccc', '<Cmd>CodeCompanionChat<CR>', { desc = '[C]ode[C]ompanion [C]hat' })
 
 -- Strudel
 keymap('n', '<leader><C-s>l', '<Cmd>StrudelLaunch<CR>', { desc = 'Strudel Launch' })
