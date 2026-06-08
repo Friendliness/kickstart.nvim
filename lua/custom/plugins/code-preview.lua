062defbf62c67fae81b0af60deabcf63dbfa2b3e
Friendliness <friendliness@friendliness.me>
2026-06-08 21:11:49 +0200

    changed to splits

--- lua/custom/plugins/code-preview.lua
+++ lua/custom/plugins/code-preview.lua
@@ -1,10 +1,9 @@
 return {
   'Cannon07/code-preview.nvim',
-
   config = function()
     require('code-preview').setup {
       diff = {
-        layout = 'tab', -- or "vsplit" / "inline"
+        layout = 'vsplit', -- or "vsplit" / "inline"
       },
       neo_tree = {
         reveal = false,
