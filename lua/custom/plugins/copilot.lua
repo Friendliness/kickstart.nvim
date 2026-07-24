4653ef1c499feb9df2fc3c80e7e328e414a8fe14
Friendliness <friendliness@friendliness.me>
2026-07-24 09:03:03 +0200

    copilot accept line

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_set_keymap('i', '<M-l>', 'copilot#Accept("<CR>")', { expr = true, noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-M-l>', 'copilot#AcceptLine()', { expr = true, noremap = true, silent = true })
 vim.g.copilot_no_tab_map = true
 vim.cmd [[autocmd! BufRead,BufNewFile *.env,*.env.*,*.env-*,.env* set filetype=dotenv]]
 return {
